c3f9b2041d0257605dd0cdcd76edd14f985c452a
jlisita <lisita_julien@hotmail.fr>
2024-03-30 18:40:03 +0100

    create grid of rows

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const containerDiv = document.querySelector(".container");
 
+let nbrColumn = 8;
+let nbrRow = 8;
 
-let rowSize = 8;
-
+let grid = [];
 let row = [];
 
-rowElement = document.createElement("div");
-
-
-rowElement.className = "row";
-containerDiv.appendChild(rowElement);
-
-for(let i = 0; i < rowSize; i++)
+for(let j = 0; j < nbrRow; j++)
 {
-    let squareEl = document.createElement("div");
-    console.log(squareEl.className);
-    squareEl.classList.add("square");
-    rowElement.appendChild(squareEl);
-    row.push(squareEl);
-}
+    rowElement = document.createElement("div");
+    rowElement.className = "row";
+    containerDiv.appendChild(rowElement);
+    grid[j]= rowElement;
 
+    for(let i = 0; i < nbrColumn; i++)
+    {
+        let squareEl = document.createElement("div");
+        console.log(squareEl.className);
+        squareEl.classList.add("square");
+        rowElement.appendChild(squareEl);
+        row.push(squareEl);
+    }
+}
